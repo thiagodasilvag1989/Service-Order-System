41d1d564b762b80d9b3ac3a7e6f8e5bd2a65fd4b
Thiago da Silva <thiagodasilvag1989@gmail.com>
2021-09-15 15:21:17 -0300

    CHANGE: Add Pacote BootStrap e gitignore

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1 = 7;
+            double valor2 = -5;
+            double valor3 = 6;
+            double valor4 = -3.4;
+            double valor5 = 4.6;
+            double valor6 = 12;
+
+            int ehPositivo = 0;
+
+            double[] todosValores = { valor1, valor2, valor3, valor4, valor5, valor6 };
+            for (int i = 0; i < todosValores.Length; i++)
+            {
+                if (todosValores[i] > 0.0)
+                {
+                    ehPositivo += 1;
+                }
+            }
+            //comtinue com sua solução
+
+            Console.WriteLine(ehPositivo + " valores positivos");
+        }
+
+    }
+}
+
+
